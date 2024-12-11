623aba01aaf736e5806c5280db365b70a7c8233d
RaySidd64 <rsiddiqui2@ivytech.edu>
2024-12-10 21:47:03 -0800

    Add files via upload

--- Module_2_assign_2.java
+++ Module_2_assign_2.java
@@ -1,29 +1,12 @@
-import java.util.Scanner;
-
 public class Module_2_assign_2 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // Create two MyDate objects
+        MyDate date1 = new MyDate();
+        MyDate date2 = new MyDate(34355555133101L);
 
-        System.out.print("Enter three side of the triangle: ");
-        double side1 = input.nextDouble();
-        double side2 = input.nextDouble();
-        double side3 = input.nextDouble();
-
-        System.out.print("Enter a color: ");
-        String color = input.next();
-
-        System.out.print("Is the triangle filled (true / false)? ");
-        boolean filled = input.nextBoolean();
-
-        Triangle triangle = new Triangle(side1, side2, side3);
-        triangle.setColor(color);
-        triangle.setFilled(filled);
-
-        System.out.println(triangle.toString());
-        System.out.println("Area: " + triangle.getArea());
-        System.out.println("Perimeter: " + triangle.getPerimeter());
-        System.out.println("Color: " + triangle.getColor());
-        System.out.println("Triangle is" + (triangle.isFilled() ? "" : " not ")
-                + "filled");
+        System.out.println("Date1: " + date1.getMonth() + "/" + date1.getDay() +
+                "/" + date1.getYear());
+        System.out.println("Date2: " + date2.getMonth() + "/" + date2.getDay() +
+                "/" + date2.getYear());
     }
 }
